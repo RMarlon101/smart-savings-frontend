03de62b7ab381ecc30467d957689681124634f0b
RMarlon101 <139892017+RMarlon101@users.noreply.github.com>
2023-08-14 14:14:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const url = 'https://www.amazon.com/ref=nav_logo';
+const rl = readline.createInterface({
+  input: process.stdin,
+  output:proecrss.stdout
+});
 
-const targetProductName =[hat, shirt, shoes, socks, pants, ]
+const websites =  [
+  'https://www.amazon.com/ref=nav_logo',
+  'https://www.bestbuy.com/',
+  'https://www.walmart.com/'
+];
+
+   console.log(`Products from ${websites[index]} (${itemCategories[index]}):`);
+      productContainers.each((i, element) => {
+        const productName = $(element).find('.product-name').text();
+        const productPrice = $(element).find('.product-price').text();
+        const productUrl = $(element).find('a.product-link').attr('href');
+        
+        console.log(`- ${productName}`);
+        console.log(`  Price: ${productPrice}`);
+        console.log(`  URL: ${productUrl}`);
+        console.log();
+      });
+      console.log('\n');
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
