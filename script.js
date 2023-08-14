93fd1cbeedd629c37ff0d8cad2dde183ee105648
RMarlon101 <139892017+RMarlon101@users.noreply.github.com>
2023-08-14 13:58:18 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const url = 'https://www.amazon.com/ref=nav_logo';
+
+const targetProductName =[hat, shirt, shoes, socks, pants, ]
