575501dcf64d0103c6de56fae33c294558129c0f
RMarlon101 <139892017+RMarlon101@users.noreply.github.com>
2023-08-17 16:24:10 -0400

    Update shopping.js

--- shopping.js
+++ shopping.js
@@ -1,31 +1,28 @@
-import { constants } from "http2"
+import { constants } from "http2";
 
-const database = firebase.database().ref()
-database.on("child_added", addItem)
+const database = firebase.database().ref();
+database.on("child_added", addItem);
 
-const shopping_list = document.getElementById("shopping_l")
+const shopping_list = document.getElementById("shopping_l");
 
-function addItem(LocalStorage){
-    const row = localStorage.val()
+function addItem(localStorage) {
+    const row = localStorage.val();
     const source_val = row.source;
-    
-    
-    const post_frame = document.createElement("div")
-    const image_container = document.createElement("div")
-    const post_image = document.createElement("img")
-    const text_container = document.createElement("div")
+    const message_val = row.message;
 
-    
-    post_frame.classList.add = "post"
-    post_image.src = source_val
-    image_container.src = source_val
-    text_container.innerHTML = message_val
+    const post_frame = document.createElement("div");
+    const image_container = document.createElement("div");
+    const post_image = document.createElemenet("img");
+    const text_container = document.createElement("div");
 
-    shopping_list.appendChild(post_frame)
-    post_frame.appendChild(post_image)
-    post_frame.appendChild(text_container)
+    post_frame.className = "post";
+    post_image.src = source_val;
+    image_container.src = source_val;
+    text_container.innerHTML = message_val;
 
+    shopping_list.appendChild(post_frame);
+    post_frame.appendChild(post_image);
+    post_frame.appendChild(text_container);
+}
 
-
-};
-console.log("check your items")
+console.log("Cheack your items");
