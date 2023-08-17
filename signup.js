48bf3e84365bd711badf9e3316b542e4e46de24d
RMarlon101 <139892017+RMarlon101@users.noreply.github.com>
2023-08-17 16:29:17 -0400

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,14 +1,13 @@
 import {initializeApp} from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js'
         
-// If you enabled Analytics in your project, add the Firebase SDK for Google Analytics
+
 import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-analytics.js'
 
-// Add Firebase products that you want to use
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js'
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js'
 (function () {
     
-    // Your web app's Firebase configuration
+  
     var firebaseConfig = {
         apiKey: "AIzaSyCBqCFd_NPi2Ihg87igH8zZeKDGGKwgTLM",
         authDomain: "smartsavings-9d5b1.firebaseapp.com",
@@ -19,23 +18,23 @@ import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase
         appId: "1:198467463653:web:b0f1395bcd990f447f270c",
         measurementId: "G-56ZPB0XLHR"
     };
-    // Initialize Firebase
+   
     initializeApp(firebaseConfig);
 
-    //Get Elements
+  
     const txtEmail = document.getElementById("txtEmail");
     const txtPassword = document.getElementById("txtPassword");
     const btnLogin = document.getElementById("btnLogin");
     const btnSignup = document.getElementById("btnSignup");
 
-    //Add Login Event
+   
     btnLogin.addEventListener('click', e => {
         const email = txtEmail.value;
         const password = txtPassword.value;
 
         const auth = getAuth();
 
-        //sign in with firebase auth
+        
         signInWithEmailAndPassword(auth, email, password).then(user =>{
             localStorage.setItem("uid", user.uid)
             alert("Login Successful :)");
@@ -46,16 +45,16 @@ import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase
         
     });
 
-    //Add Signup Event
+
     btnSignup.addEventListener('click', e => {
 
-        //get email and password
+     
         const email = txtEmail.value;
         const password = txtPassword.value;
 
         const auth = getAuth();
 
-        //sign in with firebase auth
+     
         const promise = createUserWithEmailAndPassword(auth, email, password).then(user => {
             localStorage.setItem("uid", user.uid)
             loca.Storage.setItem("currCart", [])
